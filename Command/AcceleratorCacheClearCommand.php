7790742e6dfcf672d93c83cba9ebc7eb28200d53
Kevin Bond <kevinbond@gmail.com>
2015-05-23 10:04:21 -0400

    added cache clearer service

--- Command/AcceleratorCacheClearCommand.php
+++ Command/AcceleratorCacheClearCommand.php
@@ -2,25 +2,22 @@
 
 namespace SmartCore\Bundle\AcceleratorCacheBundle\Command;
 
+use SmartCore\Bundle\AcceleratorCacheBundle\AcceleratorCacheClearer;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Loads initial data
- */
 class AcceleratorCacheClearCommand extends ContainerAwareCommand
 {
     /**
-     * @see Command
+     * {@inheritdoc}
      */
     protected function configure()
     {
         $this
             ->setName('cache:accelerator:clear')
             ->setDescription('Clears PHP Accelerator opcode and user caches.')
-            ->setDefinition(array())
             ->addOption('opcode', null, InputOption::VALUE_NONE, 'Clear only opcode cache')
             ->addOption('user', null, InputOption::VALUE_NONE, 'Clear only user cache')
             ->addOption('cli', null, InputOption::VALUE_NONE, 'Only clear the cache via the CLI')
@@ -29,151 +26,26 @@ class AcceleratorCacheClearCommand extends ContainerAwareCommand
     }
 
     /**
-     * @see Command
+     * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $clearOpcode = $input->getOption('opcode') || !$input->getOption('user');
         $clearUser = $input->getOption('user') || !$input->getOption('opcode');
-        $cli = $input->getOption('cli');
+        $type = 'Web';
 
-        if ($cli) {
-            $result = $this->clearCliCache($clearUser, $clearOpcode);
-
-            if($result['success']) {
-                $output->writeln('Cli: '.$result['message']);
-            } else {
-                throw new \RuntimeException($result['message']);
-            }
-
-            return;
-        }
-
-        $container = $this->getContainer();
-
-        $webDir = $container->getParameter('accelerator_cache.web_dir');
-
-        if (!is_dir($webDir)) {
-            throw new \InvalidArgumentException(sprintf('Web dir does not exist "%s"', $webDir));
-        }
-
-        if (!is_writable($webDir)) {
-            throw new \InvalidArgumentException(sprintf('Web dir is not writable "%s"', $webDir));
-        }
-
-        $filename = 'apc-'.md5(uniqid().mt_rand(0, 9999999).php_uname()).'.php';
-        $file = $webDir.'/'.$filename;
-
-        $templateFile = __DIR__.'/../Resources/template.tpl';
-        $template = file_get_contents($templateFile);
-        $code = strtr($template, array(
-            '%user%' => var_export($clearUser, true),
-            '%opcode%' => var_export($clearOpcode, true)
-        ));
-
-        if (false === $handle = fopen($file, 'w+')) {
-            throw new \RuntimeException(sprintf('Can\'t open "%s"', $file));
-        }
-
-        fwrite($handle, $code);
-        fclose($handle);
-
-        if (!$host = $container->getParameter('accelerator_cache.host')) {
-            $host = sprintf("%s://%s", $container->getParameter('router.request_context.scheme'), $container->getParameter('router.request_context.host'));
-        }
-
-        $url = $host.'/'.$filename;
-        $auth = $input->getOption('auth');
-
-        if ($container->getParameter('accelerator_cache.mode') == 'fopen') {
-            $context = null;
-            if (false === is_null($auth)) {
-                $context = stream_context_create(array('http' => array(
-                    'header' => 'Authorization: Basic ' . base64_encode($auth),
-                )));
-            }
-
-            $result = false;
-            for ($i = 0; $i < 5; $i++) {
-                if ($result = @file_get_contents($url, false, $context)) {
-                    break;
-                } else {
-                    sleep(1);
-                }
-            }
-
-            if (!$result) {
-                unlink($file);
-                throw new \RuntimeException(sprintf('Unable to read "%s", does the host locally resolve?', $url));
-            }
-        }
-        else {
-            $ch = curl_init($url);
-
-            $curlOpts = $container->getParameter('accelerator_cache.curl_opts');
-            curl_setopt_array($ch, array_replace($curlOpts, array(
-                CURLOPT_HEADER => false,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_FAILONERROR => true
-            )));
-
-            if (false === is_null($auth)) {
-                curl_setopt($ch, CURLOPT_USERPWD, $auth);
-            }
-
-            $result = curl_exec($ch);
-
-            if (curl_errno($ch)) {
-                $error = curl_error($ch);
-                curl_close($ch);
-                unlink($file);
-                throw new \RuntimeException(sprintf('Curl error reading "%s": %s', $url, $error));
-            }
-            curl_close($ch);
-        }
-
-        $result = json_decode($result, true);
-        unlink($file);
-
-        if($result['success']) {
-            $output->writeln('Web: '.$result['message']." Reset attempts: ".(empty($i) ? 1 : $i+1).'.');
+        if ($input->getOption('cli')) {
+            $type = 'cli';
+            $result = AcceleratorCacheClearer::clearCache($clearUser, $clearOpcode);
         } else {
-            $output->writeln('Accelerator Cache clear status: failure.');
-        }
-    }
-
-    protected function clearCliCache($clearUser, $clearOpcode)
-    {
-        $success = true;
-        $message = '';
-
-        if (function_exists('apc_clear_cache')) {
-            if ($clearUser) {
-                if (function_exists('apc_clear_cache') && version_compare(PHP_VERSION, '5.5.0', '>=') && apc_clear_cache()) {
-                    $message .= ' APC User Cache: success.';
-                } elseif (function_exists('apc_clear_cache') && version_compare(PHP_VERSION, '5.5.0', '<') && apc_clear_cache('user')) {
-                    $message .= ' APC User Cache: success.';
-                } elseif (function_exists('wincache_ucache_clear') && wincache_ucache_clear()) {
-                    $message .= ' Wincache User Cache: success.';
-                } else {
-                    $success = false;
-                    $message .= ' User Cache: failure';
-                }
-            }
-
-            if ($clearOpcode) {
-                if (function_exists('opcache_reset') && opcache_reset()) {
-                    $message .= ' Zend OPcache: success.';
-                } elseif (function_exists('apc_clear_cache') && version_compare(PHP_VERSION, '5.5.0', '<') && apc_clear_cache('opcode')) {
-                    $message .= ' APC Opcode Cache: success.';
-                }
-                else {
-                    $success = false;
-                    $message .= ' Opcode Cache: failure.';
-                }
-            }
+            $result = $this->getContainer()->get('accelerator_cache.clearer')
+                ->clearCache($clearUser, $clearOpcode, $input->getOption('auth'));
         }
 
-        return array('success' => $success, 'message' => $message);
+        if (!$result['success']) {
+            throw new \RuntimeException($result['message']);
+        }
+
+        $output->writeln(sprintf('(%s) %s', $type, $result['message']));
     }
 }
--- DependencyInjection/AcceleratorCacheExtension.php
+++ DependencyInjection/AcceleratorCacheExtension.php
@@ -2,8 +2,10 @@
 
 namespace SmartCore\Bundle\AcceleratorCacheBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Symfony\Component\DependencyInjection\Loader;
 
 class AcceleratorCacheExtension extends ConfigurableExtension
 {
@@ -12,9 +14,19 @@ class AcceleratorCacheExtension extends ConfigurableExtension
      */
     protected function loadInternal(array $mergedConfig, ContainerBuilder $container)
     {
-        $container->setParameter('accelerator_cache.host', $mergedConfig['host'] ?: trim($mergedConfig['host'], '/'));
+        $resources = __DIR__.'/../Resources';
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator($resources));
+        $loader->load('services.xml');
+
+        if (null === $host = $mergedConfig['host']) {
+            $host = '%router.request_context.scheme%://%router.request_context.host%';
+        }
+
+        $container->setParameter('accelerator_cache.host', trim($host, '/'));
         $container->setParameter('accelerator_cache.web_dir', $mergedConfig['web_dir']);
         $container->setParameter('accelerator_cache.mode', $mergedConfig['mode']);
+        $container->setParameter('accelerator_cache.template', file_get_contents($resources.'/template.tpl'));
 
         $curlOpts = array();
 
